fec4ee6ccced302596852a5a4f9d320282cf4bee
Ludvig Basmajyan <lbasmajyan@gmail.com>
2014-10-06 10:45:15 -0700

    Snake (End of 10/6)
    Added more variables and added some functions.

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -1,1 +1,18 @@
 var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementByID("game-sreen");
+    context = canvas.getContext("2d");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}

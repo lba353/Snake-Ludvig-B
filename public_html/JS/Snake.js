2356a46052adef8bc8a5c7c7d2beeaf51f301db1
Ludvig Basmajyan <lbasmajyan@gmail.com>
2014-10-07 09:21:34 -0700

    Snake (End of 10/7)
    Added a background color to my page and got rid of all the white border lines using a block element.

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -4,9 +4,18 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
-    var canvas = document.getElementByID("game-sreen");
+    var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -14,5 +23,6 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rgb(27, 230, 16)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
